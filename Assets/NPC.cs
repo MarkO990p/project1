88e156869df85dd1c591ae589702c4ef57080949
MarkO990p <mark5555thip@gmail.com>
2025-04-06 01:22:55 +0700

    Upadate
    LastWar

---
+++ Assets/NPC.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+    public string[] dialogue;
+    private int index;
+
+    public GameObject contButton;
+    public float wordSpeed;
+
+    private bool isTyping = false;
+
+    void Update()
+    {
+        // แสดงปุ่มถ้าแสดงข้อความครบแล้ว
+        if (dialogueText.text == dialogue[index] && !isTyping)
+        {
+            contButton.SetActive(true);
+        }
+    }
+
+    public void zeroText()
+    {
+        StopAllCoroutines();
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+        isTyping = false;
+    }
+
+    public void NextLine()
+    {
+        contButton.SetActive(false);
+
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText();
+        }
+    }
+
+    IEnumerator Typing()
+    {
+        isTyping = true;
+        dialogueText.text = "";
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+        isTyping = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            dialoguePanel.SetActive(true);
+            index = 0;
+            StartCoroutine(Typing());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            zeroText();
+        }
+    }
+}
